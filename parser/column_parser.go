f679fe1eec4b520e268ea3c817d0382495ad0e5c
toledompm <toledompm@gmail.com>
2021-01-28 22:19:15 -0300

    ignore editor files

---
+++ parser/column_parser.go
@@ -0,0 +1,7 @@
+package parser
+
+import "github.com/tealeg/xlsx/v3"
+
+func parseColumn(column *xlsx.Col, parseOptions ColumnParseOptions) (*xlsx.Col, error) {
+	return nil, nil
+}
--- parser/parser.go
+++ parser/parser.go
@@ -2,17 +2,34 @@ package parser
 
 import (
 	"github.com/tealeg/xlsx/v3"
-	"github.com/toledompm/kroton-etl-go/helper"
+	"github.com/toledompm/kroton-etl-go/util"
 )
 
 /*
 SheetParseOptions are passed to the parse module to define what will be changed.
-headerDictionary: map used to translate file header, keys = old values, values = new values
+
+headerDictionary: map used to translate file header. **Keys will be normalized**
+
 colParseOptions: map containing parse options for each column. Columns are identified by their header (map keys).
 */
 type SheetParseOptions struct {
-	headerDictionary map[string]string
-	colParseOptions  map[string]int
+	headerDictionary   map[string]string
+	colParseOptionsMap map[string]ColumnParseOptions
+}
+
+/*
+ColumnParseOptions will be applied to each cell contained inside a given column.
+
+colType: Excel data type to be applied
+
+colDictionary: map used to translate every cell value contained in the column. **Keys will be normalized**
+
+callback: custom function
+*/
+type ColumnParseOptions struct {
+	colType       string
+	colDictionary map[string]string
+	callback      func()
 }
 
 func readFirstSheet(filePath string) (*xlsx.Sheet, error) {
@@ -24,11 +41,10 @@ func readFirstSheet(filePath string) (*xlsx.Sheet, error) {
 
 	sheet := workBook.Sheets[0]
 
-	return sheet, err
+	return sheet, nil
 }
 
 func translateHeader(header *xlsx.Row, dict map[string]string) (*xlsx.Row, map[string]int, error) {
-
 	columnIndexes := make(map[string]int)
 
 	header.ForEachCell(
@@ -39,7 +55,7 @@ func translateHeader(header *xlsx.Row, dict map[string]string) (*xlsx.Row, map[s
 				return err
 			}
 
-			newColName := dict[helper.Normalize(colName)]
+			newColName := dict[util.Normalize(colName)]
 
 			if newColName == "" {
 				newColName = colName
@@ -59,5 +75,4 @@ func translateHeader(header *xlsx.Row, dict map[string]string) (*xlsx.Row, map[s
 
 //Parse parses a xlsx file located at FilePath based on ParseOptions
 func Parse(filePath string, parseOptions SheetParseOptions) {
-
 }
